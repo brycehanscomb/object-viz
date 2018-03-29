4429728aa25f22084afbd3c7a7fa9884fd748a5c
Bryce Hanscomb <bryce@brycehanscomb.com>
2018-03-29 12:56:46 +1100

    add more camera correction data

--- camera-data.js
+++ camera-data.js
@@ -33,13 +33,29 @@ export default [
         position: [36, 0],
         viewLeft: [0, 43],
         viewRight: [0, 0],
-        active: true
+        active: true,
+        corrections: [
+            {"apparentPosition":7.0588235294117645,"actualPosition":5.7},
+            {"apparentPosition":22.058823529411764,"actualPosition":20.05},
+            {"apparentPosition":25,"actualPosition":25},
+            {"apparentPosition":38.529411764705884,"actualPosition":38.529411764705884},
+            {"apparentPosition":93.52941176470588,"actualPosition":93.52941176470588},
+            {"apparentPosition":45.88235294117647,"actualPosition":45.88235294117647}
+        ]
     },
     {
         position: [61, 0],
         viewLeft: [67, 3],
         viewRight: [16, 99],
-        active: true
+        active: true,
+        corrections: [
+            {"apparentPosition":50.664136622390885,"actualPosition":13.15},
+            {"apparentPosition":7.2106261859582546,"actualPosition":1.3},
+            {"apparentPosition":39.658444022770404,"actualPosition":8.35},
+            {"apparentPosition":66.60341555977229,"actualPosition":30.05},
+            {"apparentPosition":59.772296015180274,"actualPosition":18.2},
+            {"apparentPosition":71.72675521821633,"actualPosition":46.4}
+        ]
     },
     {
         position: [49, 40],
@@ -57,7 +73,12 @@ export default [
         position: [52.2, 100],
         viewLeft: [99.5, 53],
         viewRight: [99.5, 95],
-        active: true
+        active: true,
+        corrections: [
+            {"apparentPosition":44.70588235294118,"actualPosition":44.70588235294118},
+            {"apparentPosition":77.3529411764706,"actualPosition":83.05},
+            {"apparentPosition":16.470588235294116,"actualPosition":23.35}
+        ]
     },
     {
         position: [73, 100],
